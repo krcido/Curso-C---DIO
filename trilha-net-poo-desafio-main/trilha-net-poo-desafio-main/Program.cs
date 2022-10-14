0cb904deb2103b1fde8fe05483ed3995f6f1c97b
Rafael Rodrigues <113770221+krcido@users.noreply.github.com>
2022-10-14 14:33:27 -0300

    Projeto POO curso C# - DIO
    Desafio resolvido em 14/10/2022.

---
+++ trilha-net-poo-desafio-main/trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "15991000000", modelo: "AA34", 
+                             imei: "1234567891011", memoria:64);
+Smartphone iphone = new Iphone(numero: "15991000000", modelo: "iPhone 13", 
+                             imei:"1110987654321", memoria:512);
+
+Console.WriteLine("Smartphone Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("=======================================");
+
+Console.WriteLine("Smartphone iPhone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
